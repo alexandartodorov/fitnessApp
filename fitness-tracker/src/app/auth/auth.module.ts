784706a525a0f4873a503bc72a5ca995f065e38f
aleanto <alexandar.a.todorov@gmail.com>
2023-12-05 22:07:26 +0200

    Add Shared Module

--- fitness-tracker/src/app/auth/auth.module.ts
+++ fitness-tracker/src/app/auth/auth.module.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '../material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { getAuth, provideAuth } from '@angular/fire/auth';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
-    provideAuth(() => getAuth()),
-  ],
-  exports: [],
+  imports: [SharedModule, ReactiveFormsModule, provideAuth(() => getAuth())],
 })
 export class AuthModule {}
--- fitness-tracker/src/app/training/training.module.ts
+++ fitness-tracker/src/app/training/training.module.ts
@@ -4,11 +4,8 @@ import { StopTrainingComponent } from './current-training/modals/stop-training.c
 import { NewTrainingComponent } from './new-training/new-training.component';
 import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
 import { TrainingComponent } from './training.component';
-import { MaterialModule } from '../material.module';
-import { CommonModule } from '@angular/common';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +15,6 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
     PastTrainingsComponent,
     StopTrainingComponent,
   ],
-  imports: [CommonModule, FormsModule, MaterialModule, FlexLayoutModule, provideFirestore(() => getFirestore())],
-  exports: [],
+  imports: [SharedModule, provideFirestore(() => getFirestore())],
 })
 export class TrainingModule {}
